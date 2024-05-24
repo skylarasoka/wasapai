169e64d4f79293fa5e50128368004df7ed76f529
Skylar <57451015+skylarasoka@users.noreply.github.com>
2024-05-24 15:28:42 +0000

    added sqlalchemy configuration on init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from .models import db 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://messages.db'   
+
+    db.init_app(app)
+    
     with app.app_context():
+        db.create_all()
+        
         from . import routes
         app.register_blueprint(routes.bp)
+    
     return app
